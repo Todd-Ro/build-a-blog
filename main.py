29e3e7bed7ac15fca7636571ccb05ca8d927eb59
Todd Romkema <todd.romkema@gmail.com>
2017-10-12 20:22:34 -0500

    Got get it done functionality working

--- main.py
+++ main.py
@@ -3,44 +3,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:get-it-done@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    archived = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
-        self.completed = False
+        self.archived = False
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
+        post_name = request.form['blog_post']
+        new_post = Post(post_name)
+        db.session.add(new_post)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html', title="Get It Done", 
-        tasks=tasks, completed_tasks=completed_tasks)
+    posts = Post.query.filter_by(archived=False).all()
+    archived_posts = Post.query.filter_by(archived=True).all()
+    return render_template('blog.html', title="Build a Blog", 
+        posts=posts, archived_posts=archived_posts)
 
 
-@app.route("/delete-task", methods=['POST'])
-def delete_task():
+@app.route("/archive-post", methods=['POST'])
+def archive_post():
      
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
+    post_id = int(request.form['post-id'])
+    post = Post.query.get(post_id)
+    post.archived = True
+    db.session.add(post)
     db.session.commit()
 
     return redirect('/')
